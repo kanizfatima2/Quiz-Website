de783603f06919cb1760e88fac3f76223bc6fa48
kanizfatima2 <kaniz686928@gmail.com>
2022-10-12 19:48:24 +0600

    s

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Questions = ({ q }) => {
-    const { options, id, question, correctAnswer } = q;
+    const { options, question, correctAnswer } = q;
+
+    const showToastMessage = (value) => {
+        // console.log(value)
+        if (value === correctAnswer) {
+            toast.success('Correct Answer !', {
+                position: toast.POSITION.TOP_RIGHT
+            });
+        }
+        else {
+            toast.success('Wrong Answer !', {
+                position: toast.POSITION.TOP_RIGHT
+            });
+        }
+
+    };
     // console.log(options[0], id, question, correctAnswer)
     return (
         <div>
@@ -12,20 +29,24 @@ const Questions = ({ q }) => {
                     <div className="grid gap-10 md:gap-8 sm:p-3 md:grid-cols-2 lg:px-12 xl:px-32">
                         <div className='flex justify-center'>
                             <div class="form-check">
-                                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
+                                <input value={options[0]} onClick={e => showToastMessage(e.target.value)} class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
+
                                 <h3 class="form-check-label inline-block text-gray-100" for="flexRadioDefault2">
                                     {options[0]}
                                 </h3>
                                 {/* <h3 className="font-semibold"></h3> */}
+                                <ToastContainer />
                             </div>
+
                         </div>
 
                         <div className='flex justify-center'>
                             <div class="form-check">
-                                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
+                                <input value={options[1]} onClick={e => showToastMessage(e.target.value)} class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
                                 <h3 class="form-check-label inline-block text-gray-100" for="flexRadioDefault2">
                                     {options[1]}
                                 </h3>
+                                <ToastContainer />
                                 {/* <h3 className="font-semibold"></h3> */}
                             </div>
                             {/* <h3 className="font-semibold">{options[1]}</h3> */}
@@ -33,22 +54,23 @@ const Questions = ({ q }) => {
                         </div>
                         <div className='flex justify-center'>
                             <div class="form-check">
-                                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
+                                <input value={options[2]} onClick={e => showToastMessage(e.target.value)} class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
                                 <h3 class="form-check-label inline-block text-gray-100" for="flexRadioDefault2">
                                     {options[2]}
                                 </h3>
                                 {/* <h3 className="font-semibold"></h3> */}
                             </div>
                             {/* <h3 className="font-semibold">{options[1]}</h3> */}
-
+                            <ToastContainer />
                         </div>
                         <div className='flex justify-center'>
                             <div class="form-check">
-                                <input class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
+                                <input value={options[3]} onClick={e => showToastMessage(e.target.value)} class="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300 bg-white checked:bg-yellow-600 checked:border-yellow-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked></input>
                                 <h3 class="form-check-label inline-block text-gray-100" for="flexRadioDefault2">
                                     {options[3]}
                                 </h3>
                                 {/* <h3 className="font-semibold"></h3> */}
+                                <ToastContainer />
                             </div>
                             {/* <h3 className="font-semibold">{options[1]}</h3> */}
 
--- src/components/QuizDetails.js
+++ src/components/QuizDetails.js
@@ -6,7 +6,7 @@ const QuizDetails = () => {
     const LoadedQuestion = useLoaderData();
     const questions = LoadedQuestion.data.questions;
 
-    console.log(questions)
+    // console.log(questions)
     return (
         <div>
             <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl mt-5">WoooHoooo <span className="text-violet-400 "> Let's Start </span></h2>
